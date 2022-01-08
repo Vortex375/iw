2b46a81473ef07f6c6d18087f93c3d680c7d3c56
Benjamin Schmitz <vortex@wolpzone.de>
2022-01-08 01:01:10 +0100

    ensure log message is not undefined

--- src/modules/deepstream-server/log-adapter.ts
+++ src/modules/deepstream-server/log-adapter.ts
@@ -48,7 +48,7 @@ class LogAdapter extends DeepstreamPlugin implements DeepstreamLogger {
   }
 
   private log(level: LOG_LEVEL, event: string, message: string, metaData: MetaData = {}) {
-    const messageEscaped = XRegExp.replace(message, UNPRINTABLE_PATTERN, '');
+    const messageEscaped = XRegExp.replace(message ?? '', UNPRINTABLE_PATTERN, '');
     switch (level) {
       case LOG_LEVEL.DEBUG:
         serverLog.debug(metaData, `${event} | ${messageEscaped || ''}`);
